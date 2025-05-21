9f7d522cf53d9f4bde55cbf0c7af314b33422e05
Carlos Regino Villalobos Espinosa <carlosvillalobos156@ub.edu>
2025-05-21 16:43:26 +0200

    Se simplifica el code para odenamiento de corpus de factiva.

--- scripts/01. crear_base.R
+++ scripts/01. crear_base.R
@@ -22,52 +22,42 @@ colnames(dat) = c("class", "text") #Nombrar columnas
 factiva <- dat %>% 
   drop_na(text) %>% 
   fill(class, .direction = "down") %>% 
-  mutate(id = ifelse(str_detect(class,"HD"),row_number(), NA),
-         class = str_replace_all(class, "[^[A-Z]]", "")) %>% 
-  fill(id) %>% 
-  pivot_wider(names_from = class, values_from = text) %>%   
-  mutate(LP = sapply(LP, toString),
-         HD = sapply(HD, toString),
-         TD = sapply(TD, toString),
-         BY = sapply(BY, toString),
-         WC = sapply(WC, toString),
-         SN = sapply(SN, toString),
-         SC = sapply(SC, toString),
-         NS = sapply(NS, toString),
-         RE = sapply(RE, toString),
-         AN = sapply(AN, toString),
-         IN = sapply(IN, toString),
-         CO = sapply(CO, toString),
-         SE = sapply(SE, toString),
-         PD = sapply(PD, toString),
-         fref = str_extract(RE, "[^:]+"),
-         date = dmy(PD),
-         date = ifelse(is.na(date), as.Date(as.numeric(PD), origin = "1899-12-30"), date)) %>% 
-  distinct(AN, HD, LP, .keep_all = T)
-
-factiva_limpia <- dat %>%
-  drop_na(text) %>%
-  fill(class, .direction = "down") %>%
   mutate(
     id = ifelse(str_detect(class, "HD"), row_number(), NA),
     class = str_replace_all(class, "[^[A-Z]]", "")
-  ) %>%
-  fill(id) %>%
+  ) %>% 
+  fill(id) %>% 
+  group_by(id) %>% 
+  mutate(row = row_number()) %>%
   pivot_wider(
+    id_cols = id,
     names_from = class,
     values_from = text,
-    values_fn = ~ .x[1]  # tomar el primer valor si hay duplicados
+    values_fn = list  # así cada columna es lista de textos por noticia
   ) %>%
+  ungroup() %>%
+  # Ahora colapsa cada lista en un solo string (separado por espacio; puedes poner salto de línea si prefieres)
+  mutate(across(
+    c(HD, LP, TD, BY, WC, SN, SC, NS, RE, AN, IN, CO, SE, PD),
+    ~map_chr(., ~paste(.x, collapse = " "))
+  )) %>%
   mutate(
-    LP = paste(LP, TD, sep = " "),              # Junta LP y TD
-    DT = dmy(PD),                                # Intenta parsear como día-mes-año
-    DT = ifelse(is.na(DT),                      # Si falla, interpreta como fecha numérica Excel
-                as.Date(as.numeric(PD), origin = "1899-12-30"),
-                DT),
-    DT = as.Date(DT)                             # Asegura que sea tipo Date
+    fref = str_extract(RE, "[^:]+"),
+    date = dmy(PD),
+    date = ifelse(is.na(date), as.Date(as.numeric(PD), origin = "1899-12-30"), date)
   ) %>%
-  distinct(AN, HD, LP, .keep_all = TRUE) %>%
-  select(DT, SN, BY, WC, HD, LP, AN)          # Selecciona solo las columnas deseadas
+  distinct(AN, HD, LP, .keep_all = TRUE)
+
+factiva_limpia <- factiva %>%
+  mutate(
+    LP = case_when(
+      LP != "" & TD != "" ~ paste0(LP, ".", TD),
+      LP != "" ~ LP,
+      TRUE ~ TD
+    ),
+    DT = as.Date(date)
+  ) %>%
+  select(DT, SN, BY, WC, HD, LP, AN)
 
 
 write_csv(factiva_limpia, "medios.csv") #Guardarlo en csv

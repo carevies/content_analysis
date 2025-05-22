5e2d414a1db5f1e96707b44b8f5358154381bfe9
Carlos Regino Villalobos Espinosa <carlosvillalobos156@ub.edu>
2025-05-22 11:48:37 +0200

    Comienza extración de Letra Ese sobre Script de extracción de discursos de AMLO.

--- scripts/01. crear_base.R
+++ scripts/01. crear_base.R
@@ -1,4 +1,6 @@
-######### Convertir base de datos de Factiva en CVS
+######### Procesar bases de datos
+
+### MEDIOS. Datos extraídos de factiva. 
 
 rm(list = ls()) 
 
@@ -61,3 +63,91 @@ factiva_limpia <- factiva %>%
 
 
 write_csv(factiva_limpia, "medios.csv") #Guardarlo en csv
+
+### SMOS. Datos extraídos de LetraEse.
+
+Sys.setlocale("LC_TIME", "es_ES")
+
+# Paquetes ----
+if(!require("rvest")) install.packages("rvest"); library(rvest)
+if(!require("stringr")) install.packages("stringr"); library(stringr)
+if(!require("dplyr")) install.packages("dplyr"); library(dplyr)
+if(!require("readr")) install.packages("readr"); library(readr)
+if(!require("beepr")) install.packages("beepr"); library(beepr)
+if(!require("lubridate")) install.packages("lubridate"); library(lubridate)
+
+# Funciones ----
+# Negar
+`%notin%` <- negate(`%in%`) 
+
+# Scrapping ----
+amlo_url <- "https://amlo.presidente.gob.mx/secciones/version-estenografica/"
+
+drop <- read_html(amlo_url) %>% 
+  html_nodes("div.pagenavi") %>% 
+  html_nodes("a") %>% 
+  html_attr("href")
+
+# Extraer número máximo de página de forma segura
+max_page <- drop %>%
+  str_extract("[0-9]+") %>%
+  as.numeric() %>%
+  max(na.rm = TRUE)
+
+# DF que contendrá todas las observaciones
+final <- data.frame()
+
+# x = número de página y loop
+for(x in 1:max_page){
+  # urls de las páginas en dónde se guardan las versiones estenográficas
+  amlos <- read_html(paste0(amlo_url, "page/", as.character(x),"/"))
+  
+  # urls de las versiones estenográficas
+  urls <- amlos %>% 
+    html_nodes("header.entry-header") %>% 
+    html_nodes("a") %>% 
+    html_attr("href")
+  
+  # títulos
+  titles <- amlos %>% 
+    html_nodes("h2.entry-title") %>% 
+    html_text()
+  
+  # fechas
+  fechas <- 
+    as.Date.character(
+      str_replace_all(
+        paste0(str_sub(titles,1,6),"20",str_sub(titles,7,8)),
+        "\\.","-"
+      ), format = "%d-%m-%Y"
+    )
+  
+  
+  # base temporal por página
+  tempo <- data.frame(
+    url = urls,
+    título = titles,
+    fecha = fechas
+  )
+  
+  bodies <- c()
+  # for para extraer títulos y textos
+  for(i in tempo$url){
+    
+    wbpg <- read_html(i)
+    
+    body <- wbpg %>%
+      html_nodes("p") %>%
+      html_text()
+    one_body <- paste(body, collapse=" ")
+    bodies <- append(bodies, one_body)
+    
+  }
+  
+  tempo$texto <- bodies
+  tempo$loop <- as.character(x)
+  
+  # se guarda cada loop en final
+  final <- bind_rows(final, tempo)
+  rm(tempo,urls,fechas,titles,bodies,wbpg,body,one_body)
+}
